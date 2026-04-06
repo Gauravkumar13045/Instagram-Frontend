240a4b88169a09928dd1d9ee327c2d1888898c1e
Gaurav kumar <gauravkumar13045@gmail.com>
2026-04-06 16:14:06 +0530

    Post section completed

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
 
     return (
         <div>
-            {/* <Login/> */}
-            <Dashboard/>
+            <Login/>
+            {/* <Dashboard/> */}
             
 
         </div>
