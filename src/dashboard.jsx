c746a1d935cb62456c8cabb7335b7df5ef6ebb92
Gaurav kumar <gauravkumar13045@gmail.com>
2026-04-13 17:25:38 +0530

    Suggestion completed

--- src/dashboard.jsx
+++ src/dashboard.jsx
@@ -104,6 +104,42 @@ function Dashboard() {
 
     }, []);
 
+    // likes
+    const [likes, setLikes] = useState(false);
+
+    const handleLike = () => {
+        if (likes) {
+            setLikes(false);
+        } else {
+            setLikes(true);
+        }
+    };
+
+    // rightPanel
+
+    const [suggest, setSuggest] = useState([])
+    const [suggestloading, setSuggestloading] = useState(true)
+    useEffect(() => {
+        setTimeout(() => {
+            fetch("/json/suggestion.json")
+                .then(res => {
+                    if (!res.ok) {
+                        throw new Error("Check Internet connection");
+                    } else {
+                        return res.json();
+                    }
+                })
+
+                .then(data => {
+                    setSuggest(data.suggestion || [])
+                    setSuggestloading(false);
+
+                })
+                .catch(err => {
+                    setSuggestloading(false);
+                });
+        }, 2000);
+    }, []);
 
     return (
 
@@ -167,11 +203,17 @@ function Dashboard() {
                         <Link to="" className="text-white whitespace-nowrap opacity-0 -translate-x-4 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300">Create</Link>
                     </div>
 
-                    <div className="flex items-center gap-4 py-2 hover:bg-gray-800 rounded-lg">
-                        <div className=" w-10 h-10 p-2 shrink-0  rounded-full cursor-pointer bg-gray-500"><title>Profile</title></div>
-                        <Link to="" className="text-white whitespace-nowrap opacity-0 -translate-x-4 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300">Profile</Link>
-                    </div>
+                    <div className="flex items-center gap-4 py-2 hover:bg-gray-800 rounded-lg group">
 
+                        <div className="w-10 h-10 shrink-0 rounded-full overflow-hidden cursor-pointer ml-1">
+                            <img src={flower} className="w-full h-full object-cover" />
+                        </div>
+
+                        <Link to="" className="text-white whitespace-nowrap opacity-0 -translate-x-4 group-hover:opacity-100 group-hover:translate-x-0 transition-all duration-300">
+                            Profile
+                        </Link>
+
+                    </div>
 
                 </div>
 
@@ -193,12 +235,12 @@ function Dashboard() {
 
 
             {/* CENTER FEED */}
-            <div className="flex-1 max-w-150 mx-auto p-5 xl:ml-62.5 ">
+            <div className="flex-1 max-w-150 p-5 xl:mx-0 mx-auto xl:ml-62.5 ">
 
 
 
                 {/* STORIES */}
-                <div className="flex w-full gap-4 p-1 pt-2  overflow-x-auto items-center  snap-x snap-mandatory">
+                <div className="flex w-full gap-4 p-1 pt-2   overflow-x-auto items-center  snap-x snap-mandatory">
 
                     {page > 0 && (
 
@@ -232,7 +274,7 @@ function Dashboard() {
                                     alt={story.username}
                                 />
                             </div>
-                            <span className="text-white text-xs ml-0">{story.username}</span>
+                            <span className="text-white text-xs truncate max-w-22 overflow-hidden">{story.username}</span>
                         </div>
                     ))}
 
@@ -361,7 +403,7 @@ function Dashboard() {
                             <div className="bg-gray-800 h-100 rounded-xl">
                                 <img
                                     src={post.postImage}
-                                    className="w-full h-full object-cover"
+                                    className="w-full h-full object-cover rounded"
                                     alt="post"
                                 />
                             </div>
@@ -369,7 +411,8 @@ function Dashboard() {
                             <div className="block">
                                 <div >
                                     <div class="likes" className="inline-flex">
-                                        <svg aria-label="Like" className=" text-white hover:transition-transform hover:scale-110  ease-in-out  shrink-0 cursor-pointer w-10 p-2" fill="currentColor" role="img" viewBox="0 0 24 24"><title>Like</title><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path></svg>
+                                        <svg aria-label="Like" onClick={handleLike} className=" text-white hover:transition-transform hover:scale-110  ease-in-out  shrink-0 cursor-pointer w-10 p-2" fill="currentColor" role="img" viewBox="0 0 24 24"><title>Like</title><path d="M16.792 3.904A4.989 4.989 0 0 1 21.5 9.122c0 3.072-2.652 4.959-5.197 7.222-2.512 2.243-3.865 3.469-4.303 3.752-.477-.309-2.143-1.823-4.303-3.752C5.141 14.072 2.5 12.167 2.5 9.122a4.989 4.989 0 0 1 4.708-5.218 4.21 4.21 0 0 1 3.675 1.941c.84 1.175.98 1.763 1.12 1.763s.278-.588 1.11-1.766a4.17 4.17 0 0 1 3.679-1.938m0-2a6.04 6.04 0 0 0-4.797 2.127 6.052 6.052 0 0 0-4.787-2.127A6.985 6.985 0 0 0 .5 9.122c0 3.61 2.55 5.827 5.015 7.97.283.246.569.494.853.747l1.027.918a44.998 44.998 0 0 0 3.518 3.018 2 2 0 0 0 2.174 0 45.263 45.263 0 0 0 3.626-3.115l.922-.824c.293-.26.59-.519.885-.774 2.334-2.025 4.98-4.32 4.98-7.94a6.985 6.985 0 0 0-6.708-7.218Z"></path></svg>
+                                        {/* <svg aria-label="Unlike" onClick={handleLike} className=" text-white hover:transition-transform hover:scale-110  ease-in-out  shrink-0 cursor-pointer w-10 p-2" fill="currentColor" role="img" viewBox="0 0 48 48"><title>Unlike</title><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg> */}
                                         <a className="text-white font-medium mt-2 cursor-pointer">{post.likes}<span>K</span></a>
                                     </div>
 
@@ -420,8 +463,134 @@ function Dashboard() {
             </div>
 
             {/* RIGHT PANEL */}
-            <div className="hidden xl:block  w-87.5 p-4 ">
-                <h1 className="text-red-500">Right Side panel</h1>
+            <div className="hidden xl:block mt-5 w-88 p-5   ">
+
+                {suggestloading && (
+                    <>
+                        {[...Array(5)].map((_, index) => (
+                            <div key={index} className="flex justify-between items-center w-full p-2 animate-pulse">
+
+                                <div className="flex items-center gap-3">
+
+
+                                    <div className="w-10 h-10 bg-gray-700 rounded-full"></div>
+
+                                    <div className="space-y-1">
+
+                                        <div className="h-3 bg-gray-700 rounded w-20"></div>
+
+                                        <div className="h-3 bg-gray-800 rounded w-16"></div>
+                                    </div>
+
+                                </div>
+
+
+                                <div className="h-3 w-12 bg-gray-700 rounded"></div>
+
+                            </div>
+                        ))}
+                    </>
+                )}
+
+
+
+
+
+                {!suggestloading && (
+                    <>
+                        <div className="flex justify-between items-center w-full p-2" >
+
+                            <div className="flex items-center gap-3">
+                                <div className="w-11 h-11 rounded-full overflow-hidden">
+                                    <img src={flower} className="w-full h-full object-cover" />
+                                </div>
+
+                                <div>
+                                    <p className="text-white text-sm font-semibold">chaitanya_raj</p>
+                                    <p className="text-gray-400 text-xs">its_me_chaitanya</p>
+                                </div>
+                            </div>
+
+                            <a className="text-blue-400  text-xs font-semibold cursor-pointer hover:text-blue-300 hover:underline underline-blue-300">
+                                Switch
+                            </a>
+
+                        </div>
+
+                        <div className="flex justify-between items-center w-full p-2 pt-2">
+
+                            <p className="text-white text-sm font-semibold">
+                                Suggested for you
+                            </p>
+
+                            <a className="text-white text-xs font-semibold hover:text-gray-500 cursor-pointer">
+                                See all
+                            </a>
+
+                        </div>
+                    </>
+                )}
+
+
+
+                {!suggestloading && !error && suggest.map((suggest, index) => (
+
+
+
+                    <div className="flex justify-between items-center w-full p-2" key={index}>
+
+                        <div className="flex items-center gap-3">
+                            <img src={suggest.SuggestionImage} className="w-11 h-11 rounded-full" />
+
+                            <div>
+                                <p className="text-white text-sm font-semibold">{suggest.username}</p>
+                                <p className="text-gray-400 text-xs truncate max-w-40">
+                                    {suggest.IsFollowing}
+                                </p>
+                            </div>
+                        </div>
+
+                        <a className="text-blue-400 hover:text-blue-500 text-xs font-semibold cursor-pointer">
+                            Follow
+                        </a>
+
+                    </div>
+                ))
+                }
+
+
+                {!suggestloading && (
+                    <>
+                        < div className="text-gray-500 text-xs mt-6 space-y-2 leading-5" >
+
+                            <div className="flex flex-wrap gap-x-2 gap-y-1">
+                                {["About", "Help", "Press", "API", "Jobs",
+                                    "Privacy", "Terms", "Locations",
+                                    "Language", "Meta Verified"].map((item, i) => (
+                                        <span key={i} className="text-xs text-[#858686] hover:underline cursor-pointer">
+                                            {item}
+                                            {i !== 9 && <span className="text-gray-600"> •</span>}
+                                        </span>
+                                    ))}
+                            </div>
+
+                            <p className="mt-3 text-[#858686] ">
+                                © 2026 INSTAGRAM FROM META
+                            </p>
+
+                        </div>
+
+                    </>
+                )}
+
+
+                < div >
+
+
+                </div>
+
+
+
             </div>
 
         </div >
